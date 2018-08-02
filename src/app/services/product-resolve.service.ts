209bc61d535cfeca949303479be5daafe9a42c9c
Eliran Eliassy <eliran.eliassy@gmail.com>
2018-08-02 16:20:14 +0300

    app shell ssr add stateTranser to to product resolve to avoid double xhr requests

--- src/app/services/product-resolve.service.ts
+++ src/app/services/product-resolve.service.ts
@@ -1,19 +1,40 @@
-import { Injectable } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
+import { Observable, of } from 'rxjs';
 
 import { ProductService } from './product.service';
+import { tap } from '../../../node_modules/rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductResolve implements Resolve<any> {
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+    @Inject(PLATFORM_ID) private platformId,
+    private transferState: TransferState) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
     const productId = route.params['id'];
-    return this.productService.getProductById(productId);
+    const key = makeStateKey<any>(productId);
+
+    if (this.transferState.hasKey(key)) {
+      const item = this.transferState.get<any>(key, null);
+      this.transferState.remove(key);
+      return of(item);
+    } else {
+      return this.productService.getProductById(productId)
+        .pipe(
+          tap(item => {
+            if (isPlatformServer(this.platformId)) {
+              this.transferState.set(key, item);
+            }
+          })
+        );
+    }
+
   }
 
 }
